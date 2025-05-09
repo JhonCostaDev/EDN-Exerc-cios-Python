af3d6c1bec3ce45aa51ca341c77f955ca112b39a
Jonathan Costa <111503993+JhonCostaDev@users.noreply.github.com>
2025-05-09 13:42:47 +0000

    refactor: menu and functions

--- main.py
+++ main.py
@@ -25,55 +25,68 @@ def menu(dictionary):
             return user_option
     
 
-def read_files_names(dictionary):
-    aula01 = dictionary.get(0)
-    aula02 = dictionary.get(1)
-    aula03 = dictionary.get(2)
-    all = aula01 + aula02 + aula03
+# def read_files_names(dictionary):
+#     # lista
+#     aula01 = dictionary.get(0)
+#     aula02 = dictionary.get(1)
+#     aula03 = dictionary.get(2)
+#     all = aula01 + aula02 + aula03
     
-    return {key: value for key, value in enumerate(all) if value !='__pycache__'}
+#     return {key: value for key, value in enumerate(all) if value !='__pycache__'}
+#read_files_names = lambda dictionary: sum(dictionary.values(), [])
+def read_files_names(dictionary):
+   
+    list_all_files =  sum(dictionary.values(), [])
+ 
+    return {key+1: value for key, value in enumerate(list_all_files)}
 
+# here is the probleam
 def switch(option, folder, dict_all_files):
     match option:
-        case 0:
-            return f'python3 {folder[0]}/{dict_all_files.get(0)}'
         case 1:
             return f'python3 {folder[0]}/{dict_all_files.get(1)}'
         case 2:
-            return f'python3 {folder[1]}/{dict_all_files.get(2)}'
+            return f'python3 {folder[0]}/{dict_all_files.get(1)}'
         case 3:
-            return f'python3 {folder[1]}/{dict_all_files.get(3)}'
+            return f'python3 {folder[1]}/{dict_all_files.get(2)}'
         case 4:
-            return f'python3 {folder[1]}/{dict_all_files.get(4)}'
+            return f'python3 {folder[1]}/{dict_all_files.get(3)}'
         case 5:
-            return f'python3 {folder[1]}/{dict_all_files.get(5)}'
+            return f'python3 {folder[1]}/{dict_all_files.get(4)}'
         case 6:
+            return f'python3 {folder[1]}/{dict_all_files.get(5)}'
+        case 7:
             return f'python3 {folder[1]}/{dict_all_files.get(6)}'
         case 7:
             return f'python3 {folder[1]}/{dict_all_files.get(7)}'
         case 8:
             return f'python3 {folder[1]}/{dict_all_files.get(8)}'
-        case 9:
-            return f'python3 {folder[1]}/{dict_all_files.get(9)}'
         case 10:
-            return f'python3 {folder[1]}/{dict_all_files.get(10)}'
+            return f'python3 {folder[1]}/{dict_all_files.get(9)}'
         case 11:
-            return f'python3 {folder[2]}/{dict_all_files.get(11)}'
+            return f'python3 {folder[1]}/{dict_all_files.get(10)}'
         case 12:
+            return f'python3 {folder[2]}/{dict_all_files.get(11)}'
+        case 13:
             return f'python3 {folder[2]}/{dict_all_files.get(12)}'
 
 
 def main():
-    folder = "aula01/atividades", "aula02/atividades", "aula03/atividades"
+
+    class_folders = [f'{item}/atividades' for item in os.listdir() if item.startswith('aula')]
+    class_folders.sort()
+
+    # folder = "aula01/atividades", "aula02/atividades", "aula03/atividades"
+
     while True:
     
-        dict_files = {x: os.listdir(folder[x]) for x in range(len(folder))}
+        dict_files = {x: os.listdir(class_folders[x]) for x in range(len(class_folders))}
     
         dict_all_files = read_files_names(dict_files) 
         option = menu(dict_all_files) 
         print(option)
 
-        os.system(switch(option, folder, dict_all_files))
+        os.system(switch(option, class_folders, dict_all_files))
         input("Aperte Enter para continuar ou crtl + c para sair")
 
     
--- test.py
+++ test.py
@@ -1,4 +1,24 @@
 import os
 
-pasta = "aula01/atividades"
-os.system(f'python3 {pasta}/idade_condicional.py')
+
+def read_files_names(dictionary):
+   
+    list_all_files =  sum(dictionary.values(), [])
+ 
+    return {key+1: value for key, value in enumerate(list_all_files)}
+
+list_all_files = os.listdir() 
+
+class_folders = [f'{item}/atividades' for item in list_all_files if item.startswith('aula')]
+#class_folders
+
+class_folders.sort()
+
+dict_files = {x+1: os.listdir(class_folders[x]) for x in range(len(class_folders))}
+
+dict_all_files = read_files_names(dict_files)
+
+
+a = [1,2,3]
+b = [4,5,6]
+print(class_folders)
